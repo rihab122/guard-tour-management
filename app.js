27a6688c52cc8727b15c94f9090489264147bf2d
rihab122 <rehabragheb221@gmail.com>
2024-11-13 23:22:53 +0200

    Update app.js
    הצגת קובץ index.html בנתיב הראשי
     index.html מסלול זה שולח את הקובץ 
    כאשר המשתמש ניגש לנתיב הראשי (/), ומשמש להצגת הממשק הראשי של האפליקציה.

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ const path = require('path');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
